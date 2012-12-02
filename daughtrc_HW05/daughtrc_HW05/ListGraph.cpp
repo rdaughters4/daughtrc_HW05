94c81c4f8b022e1485adbfd74ea06ecafd83fed1
Ryan Daughters <daughtrc@muohio.edu>
2012-12-02 17:01:00 -0500

    ListGraph.cpp complete
    Completed and working correctly.

--- daughtrc_HW05/daughtrc_HW05/ListGraph.cpp
+++ daughtrc_HW05/daughtrc_HW05/ListGraph.cpp
@@ -8,27 +8,59 @@ ListGraph::ListGraph(int numOfNodes) {
 ListGraph::~ListGraph(){
 }
 
+// Modifier
+
+// Add a weighted, undirected edge between nodes u and v.
 void ListGraph::addEdge(NodeID u, NodeID v, EdgeWeight weight) {
-	return;
+	//taken from Cary Willard
+	EList::const_iterator iter;
+	for (iter = edgeList[u].begin(); iter != edgeList[u].end(); iter++) {
+		NWPair myPair = (*iter);
+		if (myPair.first == v) {
+			myPair.second = weight;
+			for (iter = edgeList[v].begin(); iter != edgeList[v].end(); iter++) {
+				myPair = (*iter);
+				if (myPair.first == u) {
+					myPair.second = weight;
+					return;
+				}
+			}
+		}
+	}
+	edgeList[u].push_back(std::make_pair(v, weight));
+	edgeList[v].push_back(std::make_pair(u, weight));
+	num_edges++;
 }
 
+//Inspectors
+
+// Get the weight between nodes u and v; return 0 if there is no edge.
 EdgeWeight ListGraph::weight(NodeID u, NodeID v) const {
-	return 0.0;
+	//taken from Cary Willard
+	EList::const_iterator iter;
+	for (iter = edgeList[u].begin(); iter != edgeList[u].end(); iter++) {
+		NWPair myPair = (*iter);
+		if (myPair.first == v)
+			return myPair.second;
+	}
+	return NULL;
 }
 
+// Return a list of NodeID/EdgeWeight pairs describing the nodes adjacent to edge w.
 std::list<NWPair> ListGraph::getAdj(NodeID u) const {
-	std::list<NWPair>* myList = new std::list<NWPair>();
-	return *myList;
+	return edgeList[u];
 }
 
+// Return the degree (i.e. the number of neighorbs) of node u.
 unsigned ListGraph::degree(NodeID u) const {
-	return -1;
+	return edgeList[u].size();
 }
 
+// Return the number of nodes in the graph.
 unsigned ListGraph::size() const {
-	return -1;
+	return edgeList.size();
 }
 
 unsigned ListGraph::numEdges() const {
-	return -1;
+	return num_edges;
 }
